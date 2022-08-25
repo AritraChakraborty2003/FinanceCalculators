e8215507d3f5d8bc48e2910ac1599a30735d06b9
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2022-08-25 16:18:02 +0530

    added Simple interest code from branch_SI

---
+++ simpleInterest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class simpleInterest{
+
+
+  	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Principal amount");
+		int p=sc.nextInt();
+		System.out.println("Enter Interest rate");
+		int r=sc.nextInt();
+		System.out.println("Enter time in years");
+		int t=sc.nextInt();
+
+		int sim_num=p*r*t;
+		int sim_den=100;
+                                           int sim_interest=sim_sum/sim_den;
+		System.out.println(sim_interest);
+	
+
+
+         }
+
+
+}
