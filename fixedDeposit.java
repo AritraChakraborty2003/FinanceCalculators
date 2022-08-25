e4fec7db35cd690ed1117c29d5a929169f493277
Aritra Chakraborty <aritrachakraborty314@gmail.com>
2022-08-25 17:49:44 +0530

    FixedDeposit calculator code edited

--- fixedDeposit.java
+++ fixedDeposit.java
@@ -10,8 +10,8 @@ public class  fixedDeposit{
 		double r=sc.nextDouble();
 		System.out.println("Enter time in years");
 		double t=sc.nextDouble();
-		double adj_r=r/100;
-		double Amount= p*(Math.pow((1+adj_r),t));
+		double adj_r=r/200;
+		double Amount= p*(Math.pow((1+adj_r),2*t));
 		System.out.println("Your maturity amount is "+" Rs "+String.format("%.2f", Amount));
   }
 	
